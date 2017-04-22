e4974a0573267993c59179a64ab80190ce70640a
Shawn Axsom <axs221@gmail.com>
2017-04-22 08:48:54 -0400

    Yay it is working

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,23 +1,43 @@
 (ns game-of-life.core
   (:gen-class))
 
-(defn create-columns []
-  (range 5))
+(def board-width 20)
+(def board-height 20)
 
-(defn create-new-cell []
+(defn create-columns []
+  (range board-width))
+
+(defn create-random-cell []
   (fn [x y] (int (rand 2))))
 
 (defn get-value [board x y]
-  (nth (nth board y) x) 1)
+  (cond
+    (>= x (count board)) 0
+    (< x 0) 0
+    (>= y (count (nth board 0))) 0
+    (< y 0) 0
+    :else (nth (nth board y) x)))
 
 (defn count-neighbors [board x y]
-  (int(rand 3)))
+  (reduce (fn [prev xy] (+ prev (get-value board (xy 0) (xy 1)))) 0 [
+                     [(- x 1) (- y 1)]
+                     [(- x 1) y]
+                     [(- x 1) (+ y 1)]
+                     [x (- y 1)]
+                     [x (+ y 1)]
+                     [(+ x 1) (- y 1)]
+                     [(+ x 1) y]
+                     [(+ x 1) (+ y 1)]
+                     ]))
 
-(defn determine-new-cell [board]
+(defn determine-if-cell-is-alive [board]
   (fn [x y] 
+    ; (println (get-value board x y))
+    ; (println (count-neighbors board x y))
     (let [previous-value (get-value board x y)
           alive (= previous-value 1)
           neighbors (count-neighbors board x y)]
+      ; (println x y previous-value alive neighbors)
       (cond 
         (and alive (< neighbors 2)) 0
         (and alive (or (= neighbors 2) (= neighbors 3))) 1
@@ -26,32 +46,32 @@
         :else previous-value))))
 
 (defn create-row [create-cell]
-  (fn [y] (map (fn [x] (create-cell x y)) (range 5))))
+  (fn [y] (map (fn [x] (create-cell x y)) (range board-height))))
 
 (defn create-board [create-cell]
   (map (create-row create-cell) (create-columns)))
 
 (defn get-cell [c]
-  (if (= c 1) "*" " "))
+  (if (= c 1) "X" "."))
 
 (defn print-row [row]
-  ; (reduce (fn [x y] (str x y)) row))
   (reduce (fn [x y] (str x (get-cell y))) "" row))
 
 (defn print-board [board]
   (println (reduce (fn [x y] (concat x "\n" (print-row y))) "" board)))
 
 (defn run-iteration [board]
-  (let [new-board (create-board (determine-new-cell board))]
+  (let [new-board (create-board (determine-if-cell-is-alive board))]
     (print-board new-board)
     new-board))
 
 (defn -main
   "Run game of life"
   [& args]
-  (let [board (doall (create-board (create-new-cell)))]
+  (let [board (doall (create-board (create-random-cell)))]
+    ; (println (get-value board 1 1))))
     (loop [new-board board i 1]
-      (Thread/sleep 300)
-      (if (= i 10)
+      (Thread/sleep 120)
+      (if (= i 100)
         1
-        (recur (run-iteration board) (inc i))))))
+        (recur (run-iteration new-board) (inc i))))))
